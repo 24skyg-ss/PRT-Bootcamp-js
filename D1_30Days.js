b8d2f3b6ef3ae66511eea87a3595e9c334656878
unknown <gdsebastian.24@gmail.com>
2023-06-19 10:35:54 -0500

    PTR SD1 Intro

---
+++ D1_30Days.js
@@ -0,0 +1,86 @@
+    /*
+        1. Escribe un comentario de una sola línea que siga 
+        
+        // "Los comentarios pueden hacer que el código sea legible."
+    */
+
+    /*
+        2. Escribe otro comentario que diga, "Bienvenido a 30DaysOfJavaScript"
+
+        // "Bienvenido a 30DaysOfJavaScript"
+    */
+
+    /*
+        3. Escribe un comentario multilínea que diga, "Los comentarios pueden 
+        hacer el código legible, fácil de usar e informativo"
+
+        "Los comentarios pueden hacer el código legible,
+        fácil de usar e informativo.""
+    */
+
+    /*
+        4. Crear un archivo "variable.js" y declarar variables y asignar tipos
+        de datos string, booleanos, indefinidos y nulos.
+    */
+    var cadena = "string"
+    var num = 9
+    var estado = true
+    var conf = null
+    var indef
+
+    /*
+        5. Crea el archivo datatypes.js y utilizar el operador "typeof" de JS para
+        comprobar diferentes tipos de datos. Comprueba el tipo de datos de cada
+        variable.
+    */
+    console.log(typeof cadena) // string
+    console.log(typeof num) // número
+    console.log(typeof estado) // boolean
+    console.log(typeof conf) // tipo de objeto
+    console.log(typeof indef) // undefined
+
+    /*
+        6. Declara 4 variables sin asignar valores.
+    */
+    var off1
+    var off2
+    var off3
+    var off4
+
+    /*
+        7. Declara 4 variables con valores asignados.
+    */
+    let num1 = 320
+    let num2 = 21
+    let num3 = 84
+    let num4 = 43.1
+
+    /*
+        8. Declara variables para almacenar su nombre, apellido, estado civil
+        país, y edad en múltiples líneas.
+    */
+    var nombre = 'Nombre'
+    var apellido = 'Apellido'
+    var est_Civil = 'C'
+    var pais = 'EEUU' 
+    var edad = 19 
+
+    /*
+        9. Declara variables para almacenar su nombre, apellido, estado civil
+        país, y edad en una sola línea.
+    */
+    var nombre = 'Nombre', apellido = 'Apellido', est_Civil = 'C', pais = 'EEUU', edad = 19
+
+    /*
+        10. Declara la variable miEdad y tuEdad y asígneles valores iniciales y
+        registrese en la consola del navegador.
+    */
+    var miEdad
+    var tuEdad
+    miEdad = 19
+    tuEdad = 22
+
+    console.log(miEdad)
+    console.log(tuEdad)
+   
+    
--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,17 @@
 console.log("Hola Mundo");
+/*
+    // Operaciones aritméticas
+    console.log(2 + 3) // Adición
+    console.log(3 - 2) // Sustracción
+    console.log(2 * 3) // Multiplicación
+    console.log(3 / 2) // División
+    console.log(3 % 2) // Módulo - encontrar el resto
+    console.log(3 ** 2) // Exponencial
+*/
+
+// Comprobación de tipos de datos
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // número
+console.log(typeof true ) // boolean
+console.log(typeof null) // tipo de objeto
+console.log(typeof undefined) // undefined
