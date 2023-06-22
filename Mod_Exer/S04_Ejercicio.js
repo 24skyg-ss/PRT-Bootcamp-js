1348522c9d8ff6102a7a4025a08ea219833ee543
unknown <gdsebastian.24@gmail.com>
2023-06-22 13:38:43 -0500

    PRT SD4 Strings

---
+++ Mod_Exer/S04_Ejercicio.js
@@ -0,0 +1,12 @@
+let firstName = "24skyg-ss";
+let lastName = "Doro.";
+let estudiante = firstName.concat(" ", lastName);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let length_estudiante = estudiante.length;
+let charAt_firstName = firstName.charAt(0);
+let charAt_lastName = lastName.charAt(lastName.length-2);
+let trim_estudiante = estudiante.trim();
+let isContent_estudiante = estudiante.includes(firstName);
+
+console.log(isContent_estudiante);
